785f71f8b57b881bb3d14b9a313b76349d6eed7c
Wiliam Buzatto <wiliambuzatto@gmail.com>
2018-05-02 18:34:52 -0400

    Adicionado layout do Bootswatch

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,14 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Termek.Data;
 using Termek.Models;
 
 namespace Termek.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly DataContext _context;
+
+        public HomeController(DataContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.TotalProdutos = _context.Produto.Count();
             return View();
         }
     }
